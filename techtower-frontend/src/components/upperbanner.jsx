56dcb0275d88ae7e480f4d97349e5b451fb836be
EthnicTea <ren.9000@hotmail.com>
2025-06-07 19:09:39 -0400

    feat: Dependencias actualizadas y .gitignore configurado

---
+++ techtower-frontend/src/components/upperbanner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const upperbanner = () => {
+    return (
+    <div class="shape rect rectangle-412c8972fb7f"></div>
+    )
+}
+
+export default upperbanner;
